3f328a6ae0b7d07b79f174839c0fe05628025747
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-04-18 13:09:15 -0400

    created and imported navbar component

---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <a href="" class="brand-logo left">Geo Maps Tracker</a>
+        <ul class="right">
+          <li>
+            <a href="">
+              Sign up
+            </a>
+          </li>
+          <li>
+            <a href="">
+              Log in
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
