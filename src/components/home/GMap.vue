d08c54d540b43b292e16154f3da2227d9489ea76
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-04-18 16:03:30 -0400

    vue instance initializes only after firebase auth initializes to check for user authentication status

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'GMap',
   data () {
@@ -29,6 +31,7 @@ export default {
   },
   mounted () {
     this.renderMap()
+    console.log(firebase.auth().currentUser)
   }
 }
 </script>
