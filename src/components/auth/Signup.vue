e18a5a07f3be64e08e4af3337004d711eb3e7b0d
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-04-18 15:17:34 -0400

    signup for is now functional and new users can be created in authentication firestore

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -27,6 +27,7 @@
 <script>
 import db from '@/firebase/init'
 import slugify from 'slugify'
+import firebase from 'firebase'
 
 export default {
   name: 'Signup',
@@ -41,7 +42,7 @@ export default {
   },
   methods: {
     signup () {
-      if (this.alias) {
+      if (this.alias && this.email && this.password) {
         this.slug = slugify(this.alias, {
           replacement: '-',
           remove: /[$*_+~.()'"!\-:@]/g,
@@ -52,12 +53,17 @@ export default {
           if (doc.exists) {
             this.feedback = 'This alias already exists'
           } else {
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+              .catch(err => {
+                console.log(err)
+                this.feedback = err.message
+              })
             this.feedback = 'This alias is available to use'
           }
         })
         console.log(this.slug)
       } else {
-        this.feedback = 'Please enter an alias'
+        this.feedback = 'Please fill out all fields'
       }
     }
   }
